8faeb63f6f385b5c7b46a64a7a53b9e4d0959368
Andy Goryachev <andy.goryachev@oracle.com>
2023-11-17 10:57:06 -0800

    moved rich text area to javafx.incubator.controls module

--- modules/javafx.incubator.controls/src/main/java/module-info.java
+++ modules/javafx.incubator.controls/src/main/java/module-info.java
@@ -36,6 +36,10 @@ module javafx.incubator.controls {
     requires transitive javafx.base;
     requires transitive javafx.graphics;
     requires transitive javafx.controls;
+    requires transitive java.desktop;
 
     exports javafx.incubator.scene.control;
+    exports javafx.incubator.scene.control.rich;
+    exports javafx.incubator.scene.control.rich.skin;
+    exports javafx.incubator.scene.control.rich.model;
 }
