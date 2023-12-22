411b490fdb006f070a8069c14bcc16f8dc487c50
Andy Goryachev <andy.goryachev@oracle.com>
2023-12-22 15:45:02 -0800

    styles

--- tests/manual/RichTextAreaDemo/src/com/oracle/demo/rich/rta/MultipleStackedBoxWindow.java
+++ tests/manual/RichTextAreaDemo/src/com/oracle/demo/rich/rta/MultipleStackedBoxWindow.java
@@ -115,6 +115,10 @@ public class MultipleStackedBoxWindow extends Stage {
         }
 
         Scene scene = new Scene(sp);
+        scene.getStylesheets().addAll(
+            RichTextAreaWindow.class.getResource("RichTextArea-Modena.css").toExternalForm(),
+            RichTextAreaWindow.class.getResource("RichTextAreaDemo.css").toExternalForm()
+        );
         setScene(scene);
     }
     
